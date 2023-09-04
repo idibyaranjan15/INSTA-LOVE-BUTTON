36df7afcf1ed1e50e76696acda42c7fa839a2d5a
DIBYA RANJAN SAHOO <78527632+idibyaranjan15@users.noreply.github.com>
2023-09-04 12:48:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var con=document.querySelector("#container")
+
+var love=document.querySelector("i")
+
+
+
+con.addEventListener("dblclick",()=>{
+    love.style.transform="translate(-50%,-50%) scale(2)"
+    love.style.opacity="0.8"
+    setTimeout(()=>{
+        love.style.opacity="0"
+    },1000)
+
+    setTimeout(()=>{
+        love.style.transform="translate(-50%,-50%) scale(0)"
+    },1500)
+
+})
